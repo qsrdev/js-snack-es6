5a8ac42a82743b158899c81105772e8f573cbe33
qsrdev <matteo.albertini747@gmail.com>
2025-05-05 15:18:19 +0200

    feat: snack 1 done

---
+++ snack 1/js/script.js
@@ -0,0 +1,25 @@
+const biciclette = [
+  { nome: "Speedster 100", peso: 8.5 },
+  { nome: "Mountain Pro", peso: 12.3 },
+  { nome: "City Rider", peso: 10.1 },
+  { nome: "Trail Blazer", peso: 13.7 },
+  { nome: "Road Rocket", peso: 7.9 },
+  { nome: "Eco Bike", peso: 9.8 },
+  { nome: "Gravel Master", peso: 11.4 },
+  { nome: "Urban Light", peso: 9.2 },
+  { nome: "Fat Tire King", peso: 15.6 },
+  { nome: "Classic Cruiser", peso: 10.7 },
+];
+
+let lightBike = biciclette[0].peso;
+console.log(lightBike);
+
+for (let i = 0; i < biciclette.length; i++) {
+  console.log(biciclette[i].peso);
+
+  if (biciclette[i].peso < lightBike) {
+    lightBike = biciclette[i].peso;
+  }
+}
+
+console.log(`la bicicletta più leggera pesa ${lightBike} kg`);
